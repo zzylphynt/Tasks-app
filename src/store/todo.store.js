f92f3214378ef9119d4caf26db06b18665976e66
Daniel Lopez <daniellopez7812@outlook.com>
2024-07-15 14:30:27 -0600

    all functionality created at: todo.store.js, todo.model.js modified

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -15,13 +15,70 @@ const state = {
   filter: Filters.All,
 };
 
+//*  All buttons functionality
+const initStore = () => {
+  console.log(state);
+  console.log("initStore");
+};
 
-const initStore = ( ) => {
-    console.log(state)
-    console.log('initStore')
-}
+const loadStore = () => {
+  throw new Error("Not implemented");
+};
 
+const getTodos = (filter = Filters.All) => {
+  switch (filter) {
+    case Filters.All:
+      return [...state.todos];
 
-export default{
-    initStore,
-}
+    case Filters.Completed:
+      return state.todos.filter((todo) => todo.done);
+
+    case Filters.Pending:
+      return state.todos.filter((todo) => !todo.done);
+
+    default:
+      throw new Error(`Option ${filter} is not valid`);
+  }
+};
+
+const addTodo = (description) => {
+  if (!description) throw new Error("Description is required");
+  state.todos.push(new Todo(description));
+};
+
+const toggleTodo = (todoId) => {
+  state.todos = state.todos.map((todo) => {
+    if (todo.id === todoId) {
+      todo.done !== todo.done;
+    }
+    return todo;
+  });
+};
+
+const deleteTodo = (todoId) => {
+  state.todos = state.todos.filter((todo) => todo.id !== todoId);
+};
+
+const deleteCompleted = () => {
+  state.todos = state.todos.filter((todo) => todo.done);
+};
+
+const setFilter = (newFilter = Filters.All) => {
+  state.filter = newFilter;
+};
+
+const getCurrentFilter = () => {
+  return state.filter;
+};
+
+export default {
+  addTodo,
+  deleteCompleted,
+  deleteTodo,
+  getCurrentFilter,
+  getTodos,
+  initStore,
+  loadStore,
+  setFilter,
+  toggleTodo,
+};
--- src/todos/models/todo.model.js
+++ src/todos/models/todo.model.js
@@ -1,6 +1,8 @@
+import {v4 as uuid} from "uuid";
+
 export class Todo {
   constructor(description) {
-    this.id = 1;
+    this.id = uuid();
     this.description = description;
     this.done = false;
     this.createdAt = new Date();
